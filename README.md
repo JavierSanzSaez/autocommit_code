6251b657f3022d39bf4f3985ab41faccb96eae15
Javier Sanz Sáez <javiersse@gmail.com>
2023-02-08 23:42:45 +0100

    Migrate to Typescript to add a challenge myself :D

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-![Pollo](https://img.shields.io/badge/Pollo-%F0%9F%91%8D-yellow)
 # autocommit-code README
 
 This is the README for your extension "autocommit-code". After writing up a brief description, we recommend including the following sections.
@@ -50,13 +49,19 @@ Added features X, Y, and Z.
 
 ---
 
+## Following extension guidelines
+
+Ensure that you've read through the extensions guidelines and follow the best practices for creating your extension.
+
+* [Extension Guidelines](https://code.visualstudio.com/api/references/extension-guidelines)
+
 ## Working with Markdown
 
-You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
+You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
 
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets
+* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux).
+* Toggle preview (`Shift+Cmd+V` on macOS or `Shift+Ctrl+V` on Windows and Linux).
+* Press `Ctrl+Space` (Windows, Linux, macOS) to see a list of Markdown snippets.
 
 ## For more information
 
--- src/extension.ts
+++ src/extension.ts
@@ -1,32 +1,36 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const { exec } = require('child_process');
-const vscode = require('vscode');
+import { exec } from 'child_process';
+import * as vscode from 'vscode';
+
+class FolderDiff {
+	path: string;
+	diff: string;
+
+	constructor(path:string,diff:string){
+		this.path=path;
+		this.diff=diff;
+	}
+}
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
-function activate(context) {
+export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "autocommit-code" is now active!');
 
 	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
+	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let generateCommitMessage = vscode.commands.registerCommand('autocommit-code.generateCommitMessage', function () {
+	let generateCommitMessage = vscode.commands.registerCommand('autocommit-code.generateCommitMessage', () => {
 		// The code you place here will be executed every time your command is executed
-
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Calculating diffs...');
 
-		//  Get the diff including untracked files (see https://stackoverflow.com/a/52093887)
-		let git_diffs = [];
-		let errors = []
+		let git_diffs: FolderDiff[] = [];
+		let errors: string[] = [];
 
 		let workspace_folders = vscode.workspace.workspaceFolders;
 
@@ -36,29 +40,19 @@ function activate(context) {
 
 			const bash_result = exec(git_command, function(error, stdout, stderr){
 				if (stderr){
-					errors.push(error)
+					errors.push(String(error))
 				}
 				else{
-					git_diffs.push({
-						path: path,
-						diff:stdout
-					});
+					git_diffs.push(new FolderDiff(path,stdout));
 				}
 			});
 		});
 
 		console.log(git_diffs);
-		
-	
 	});
 
 	context.subscriptions.push(generateCommitMessage);
 }
 
 // This method is called when your extension is deactivated
-function deactivate() {}
-
-module.exports = {
-	activate,
-	deactivate
-}
+export function deactivate() {}
